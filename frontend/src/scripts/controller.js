749b2e2fae36866bf07ff008b23182929962a4ac
Martin Hecher <martin@redsift.io>
2017-08-04 09:22:42 +0100

    add test call to /echo

--- frontend/src/scripts/controller.js
+++ frontend/src/scripts/controller.js
@@ -1,77 +1,28 @@
 /**
- * Botfwk Demo Sift. Frontend controller entry point.
+ * RPC Sift - A Sift demonstrating the Sift API access.
  */
+
 import { SiftController, registerSiftController } from '@redsift/sift-sdk-web';
-// import { SiftController, registerSiftController } from './sift-sdk-web/sdk';
 
-export default class MyController extends SiftController {
-  constructor() {
-    // You have to call the super() method to initialize the base class.
-    super();
-    // this._suHandler = this.onStorageUpdate.bind(this);
-  }
-
+export default class RPCSiftController extends SiftController {
   // for more info: http://docs.redsift.com/docs/client-code-siftcontroller
-  loadView(state) {
-    console.log('botfwk-sift: loadView', state);
+  loadView({ params }) {
+    console.log('[rpc-sift|controller] loadView | params:', params);
 
-    const authToken = `Bearer ${state.params.rpcJweToken.token}`;
+    const apiToken = `Bearer ${params.rpcJweToken.token}`;
+    const userAccountId = params.userAccountId;
 
-    console.log('botfwk-sift: authToken', authToken);
+    const data = {
+      apiToken,
+      userAccountId,
+    };
 
-    const oReq = new XMLHttpRequest();
-
-    oReq.addEventListener('load', () => {
-      console.log('RESPONSE:', oReq.response);
-    });
-    oReq.addEventListener('error', () => {
-      console.error('ERROR:', oReq.status);
-    });
-    oReq.setRequestHeader('Redsift-Account', state.params.userAccountId);
-    oReq.setRequestHeader('Authorization', authToken);
-    oReq.open('GET', 'https://rpc.redsift.io/domains', true);
-    oReq.send();
- 
-    // Register for storage update events on the "x" bucket so we can update the UI
-    // this.storage.subscribe(['x'], this._suHandler);
-    switch (state.type) {
-      case 'email-thread':
-        return {
-          html: 'email-thread.html',
-          data: {}
-        };
-      case 'summary':
-        return {
-          html: 'summary.html',
-          data: this.getToken()
-        };
-      default:
-        console.error('botfwk-sift: unknown Sift type: ', state.type);
-    }
+    return {
+      html: 'summary.html',
+      data,
+    };
   }
-
-  // Event: storage update
-  // onStorageUpdate(value) {
-  //   console.log('botfwk-sift: onStorageUpdate: ', value);
-  //   return this.getX().then(xe => {
-  //     // Publish events from 'x' to view
-  //     this.publish('counts', xe);
-  //   });
-  // }
-
-   getToken() {
-    return this.storage.get({
-      bucket: '_redsift',
-      keys: ['botfwk/handshake_token']
-    }).then((values) => {
-      console.log('botfwk-sift: getToken returned:', values);
-      return {
-        token: values[0].value || ""
-      };
-    });
-  }
-
 }
 
 // Do not remove. The Sift is responsible for registering its views and controllers
-registerSiftController(new MyController());
+registerSiftController(new RPCSiftController());
